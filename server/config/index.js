419b8ce923f2c8e12c80cbc7691046fb7f5d55c5
David Brown <dab0304@gmail.com>
2018-12-08 12:02:30 -0600

    moved config files into config folder from server

--- server/config/index.js
+++ server/config/index.js
@@ -15,13 +15,13 @@ app.listen(app.get('port'), function(){
 /* //////////////////////*/
 
 var express = require('express');
-var config = require('./config/config');
+var config = require('./config');
 
 var app = express();
 
 var port = process.env.port || 3000
 
-require('./config/express')(app, config);
+require('./express')(app, config);
 
 require('http').createServer(app).listen(port, function () {
 console.log("HTTP Server listening on port: %d, in %s mode", port, app.get('env'));
