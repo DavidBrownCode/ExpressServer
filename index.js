3e9fc7230cac25d3c2e7af9328c0ed1efabebd78
David Brown <dab0304@gmail.com>
2018-12-08 18:07:10 -0600

    reworking index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var express = require ('expresss');
- 
+var http = require('http');
+var express = require('express');
 var app = express();
-
 app.set('port', process.env.PORT || 3000);
-
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get('/',function(req,res){
+    res.send('Hello World!');
 });
 
-app.listen(app.get('port'), function(){
-    console.log('Express start on http://localhost' + app.get('port'));
+app.use(function(req,res,next){
+    console.log('Request from ' + req.ip);
 });
+
+/*-app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port'));
+});*/
+
+html.createServer(app).listen(3000, function(){
+    console.log('Express server listening on port ' + 3000);
+});
+
+
 
 /* //////////////////////
 
