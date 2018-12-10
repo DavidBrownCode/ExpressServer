8b5af93b83b69cd4192139a450476ee811d8660c
David Brown <dab0304@gmail.com>
2018-12-09 18:07:39 -0600

    trying to figure out what happened to  my index.js added in login.html for the third time

---
+++ ClassNotes/oldIndex.js
@@ -0,0 +1,11 @@
+var express = require ('express');
+var config = require ('../../../810/server/config/config');
+
+var app = express();
+var port = process.env.port || 3000;
+
+require('./config.express')(app, config);
+
+require('http').createServer(app).listen(port, function (){
+    console.log("HTTP Server listening on port: %d, in %s mode", port,app.get('env'));
+});
