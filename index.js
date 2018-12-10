04063ac604262da07ec5cec86c841fa203365326
David Brown <dab0304@gmail.com>
2018-12-10 15:41:14 -0600

    found out what has been wrong with index.js

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ var config = require('./config/config');
 
 var app = express();
 
-var port = process.env.port || 3000
+var port = process.env.port || 3000;
 
 require('./config/express')(app, config);
 
 require('http').createServer(app).listen(port, function () {
-console.log("HTTP Server listening on port: %d, in %s mode", port, app.get('env'));
+    logger.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
