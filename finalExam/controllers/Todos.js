fc861072d7d374b60ad2abd96980c6faa7aa9b0a
David Brown <dab0304@gmail.com>
2018-12-10 19:00:12 -0600

    altered todos.js adding in notes from file uploads class lesson notes

--- finalExam/controllers/Todos.js
+++ finalExam/controllers/Todos.js
@@ -15,14 +15,14 @@ query.sort(req.query.order)
 router.get('/users/:id', requireAuth, asyncHandler(async (req, res) => {
     logger.log('info', 'Get all ToDos');
     let query = HelpTicket.find();
-query.sort(req.query.order)
-if(req.query.status){
-if(req.query.status[0] == '-'){
-query.where('status').ne(req.query.status.substring(1));
-} else {
-query.where('status').eq(req.query.status);
-}
-}
+    query.sort(req.query.order)
+    if(req.query.status){
+        if(req.query.status[0] == '-'){
+            query.where('status').ne(req.query.status.substring(1));
+        } else {
+            query.where('status').eq(req.query.status);
+        }
+    }
     await query.exec().then(result => {
         res.status(200).json(result);
     })
@@ -33,9 +33,9 @@ query.where('status').eq(req.query.status);
 router.get('/users/:id', requireAuth, asyncHandler(async (req, res) => {
     logger.log('info', 'Get all ToDos');
     let query = HelpTicket.find();
-query.sort(req.query.order)
-.populate({path: 'personId', model: 'User', select: 'lastName firstName fullName'} )
-.populate({path: 'ownerId', model: 'User', select: 'lastName firstName fullName'} );
+    query.sort(req.query.order)
+    .populate({path: 'personId', model: 'User', select: 'lastName firstName fullName'} )
+    .populate({path: 'ownerId', model: 'User', select: 'lastName firstName fullName'} );
 }));
 
 //add schema .json files (Help Ticket API)
@@ -43,3 +43,23 @@ query.sort(req.query.order)
 /*added in from authentication lessons
 router.get('/users/:id', requireAuth, asyncHandler(async (req, res) => {
     logger.log('info', 'Get all ToDos');*/
+
+/*Added from File Uploads class notes
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    var path = config.uploads + '/helpTickets';
+    mkdirp(path, function(err) {
+            if(err){
+                res.status(500).json(err);
+            } else {    
+                cb(null, path);
+            }
+        });
+        },
+        filename: function (req, file, cb) {
+            file.fileName = file.originalname ;
+            cb(null, file.fieldname + '-' + Date.now());
+        }
+});
+*/     
