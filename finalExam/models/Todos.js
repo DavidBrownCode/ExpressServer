877d546f443c72bdc88c844f36770ee61c74fe60
David Brown <dab0304@gmail.com>
2018-12-10 19:21:10 -0600

    fixed models/Todos.js

--- finalExam/models/Todos.js
+++ finalExam/models/Todos.js
@@ -1,68 +1,9 @@
 var Mongoose = require('mongoose');
 var Schema = Mongoose.Schema;
 
-var ToDoSchema = new Schema({
+var ToDoContentSchema = new Schema({
     todo:{ type:String},
     priority: {type: String, enum: ['High', 'Medium', 'Low']},
     done: { type: Boolean, default: false}
 });
-
-/*
-//Many side Document
-{
-    Title
-    PersonID
-    OwnerID
-    Status
-    Content {
-    PersonID
-    Content
-    File {
-    FileName
-    OrignalFileName
-    }
-    DateCreated
-    }
-    DateCreated
-}
-
-//Array of many side document
-{
-    Title
-    PersonID
-    OwnerID
-    Status
-    Content [ ]
-    DateCreated
-    }
-
-    {
-    PersonID
-    Content
-    File {
-    FileName
-    OriginalFileName
-    }
-    DateCreated
-    }
-
-    //One side ID in Many Side document
-    {
-        Title
-        PersonID
-        OwnerID
-        Status
-        DateCreated
-        }
-    
-        {
-            PersonID
-            Content
-            HelpTicketID
-            File {
-                FileName
-                OriginalFileName
-                }
-            DateCreated
-        }
-*/
+module.exports = Mongoose.model('ToDosContent', ToDoContentSchema);
