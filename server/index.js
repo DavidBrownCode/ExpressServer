96671e69bb0919f497184bf9b4d3f338b2f06340
David Brown <dab0304@gmail.com>
2018-12-14 23:16:30 -0600

    repaired line 11 index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ var port = process.env.port || 5000;//change to 5000 when connecting to logger.l
 
 require('./config/express')(app, config);
 
-require('http').createServer(app).listen(port, function () {
+require('http').createServer(app).listen(config.port, function () {
     logger.log('info',"HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
 
