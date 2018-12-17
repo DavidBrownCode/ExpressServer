be4466b1417601482cbb4846b7833928a9725fb1
David Brown <dab0304@gmail.com>
2018-12-16 19:56:13 -0600

    cleaning the express.js of old notes

--- server/config/config.js
+++ server/config/config.js
@@ -28,25 +28,3 @@ var config = {
 };    
     module.exports = config[env];
 
-
-    /*  //////////////////////// Express.js Notes */ 
-
-    /*The configuration will use either the development or
-    production configuration based on the NODE_ENV
-    environment variable
-
-    var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
-    var config = {
-    development: {
-    root: rootPath,
-    app: { name: 'ExpressServer' },
-    port: 5000,
-    },
-    production: {
-    root: rootPath,
-    app: { name: 'ExpressServer' },
-    port: 80, }
-    };
-    module.exports = config[env];*/
